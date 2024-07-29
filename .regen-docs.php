99a936bc26a164b7b5db3e71249aa235fefead38
Cedric Ziel <cedric@cedric-ziel.com>
2024-07-29 20:33:26 +0000

    docs: document methods

--- .regen-docs.php
+++ .regen-docs.php
@@ -4,14 +4,40 @@ use CedricZiel\Baserow\Generated\Client;
 
 require_once __DIR__.'/vendor/autoload.php';
 
+function underscoreToCamelCase($string, $capitalizeFirstCharacter = false)
+{
+
+    $str = str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
+
+    if (!$capitalizeFirstCharacter) {
+        $str[0] = strtolower($str[0]);
+    }
+
+    return $str;
+}
+
 $methods = get_class_methods(Client::class);
 $readme = file_get_contents('./.github/README.md');
 
+$json = json_decode(file_get_contents('openapi.json'), true);
+
 $doc = '';
 
-foreach ($methods as $method) {
-    echo "$method\n";
-    $doc .= "$method\n";
+foreach ($json['paths'] as $pathName => $path) {
+    foreach ($path as $httpMethod => $method) {
+        $methodName = underscoreToCamelCase($method['operationId']);
+
+        $doc .= '### ' . $method['operationId'] . ' - ' . "$httpMethod $pathName\n";
+        $doc .= "\n";
+        $doc .= $method['description'] ?? '';
+        $doc .= "\n\n";
+        $doc .= '```php' . "\n";
+        $doc .= '$client = \\CedricZiel\\Baserow\\Client::create();' . "\n";
+        $doc .= '$client->' . "{$methodName}();\n";
+        $doc .= '```' . "\n";
+        $doc .= "\n";
+        echo $method['operationId'] . "\n";
+    }
 }
 
 file_put_contents('README.md', str_replace('### docs ###', $doc, $readme));
