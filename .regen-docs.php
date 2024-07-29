60cf3365f1093e96b6023d42c31bb65a69f1a24c
cedricziel <cedricziel@users.noreply.github.com>
2024-07-29 20:39:59 +0000

    Apply php-cs-fixer changes

--- .regen-docs.php
+++ .regen-docs.php
@@ -6,7 +6,6 @@ require_once __DIR__.'/vendor/autoload.php';
 
 function underscoreToCamelCase($string, $capitalizeFirstCharacter = false)
 {
-
     $str = str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
 
     if (!$capitalizeFirstCharacter) {
@@ -27,16 +26,16 @@ foreach ($json['paths'] as $pathName => $path) {
     foreach ($path as $httpMethod => $method) {
         $methodName = underscoreToCamelCase($method['operationId']);
 
-        $doc .= '### ' . $method['operationId'] . ' - ' . "$httpMethod $pathName\n";
+        $doc .= '### '.$method['operationId'].' - '."$httpMethod $pathName\n";
         $doc .= "\n";
         $doc .= $method['description'] ?? '';
         $doc .= "\n\n";
-        $doc .= '```php' . "\n";
-        $doc .= '$client = \\CedricZiel\\Baserow\\Client::create();' . "\n";
-        $doc .= '$client->' . "{$methodName}();\n";
-        $doc .= '```' . "\n";
+        $doc .= '```php'."\n";
+        $doc .= '$client = \\CedricZiel\\Baserow\\Client::create();'."\n";
+        $doc .= '$client->'."{$methodName}();\n";
+        $doc .= '```'."\n";
         $doc .= "\n";
-        echo $method['operationId'] . "\n";
+        echo $method['operationId']."\n";
     }
 }
 
