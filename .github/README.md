77018e9c9e9af5e27cac0f16d395488cbc8cb39a
Cedric Ziel <cedric@cedric-ziel.com>
2024-07-29 20:11:47 +0000

    docs: regen README

---
+++ .github/README.md
@@ -0,0 +1,15 @@
+# php-baserow-client
+
+An API client for baserow.io
+
+```shell
+composer require cedricziel/baserow
+```
+
+## Docs
+
+### docs ###
+
+## License
+
+Apache 2.0
---
+++ .regen-docs.php
@@ -0,0 +1,17 @@
+<?php
+
+use CedricZiel\Baserow\Generated\Client;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$methods = get_class_methods(Client::class);
+$readme = file_get_contents('./.github/README.md');
+
+$doc = '';
+
+foreach ($methods as $method) {
+    echo "$method\n";
+    $doc .= "$method\n";
+}
+
+file_put_contents('README.md', str_replace('### docs ###', $doc, $readme));
